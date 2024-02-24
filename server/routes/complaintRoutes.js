faaf43f4df589840d12a6bcf64f7aaa3191b90b4
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-02-25 00:20:32 +0500

    fix

--- server/routes/complaintRoutes.js
+++ server/routes/complaintRoutes.js
@@ -1,6 +1,7 @@
 import {
     createComplaint,
     deleteComplaint,
+    getComplaintsByBusiness,
     getComplaintsByUser
 } from "../controllers/complaint.js"
 
@@ -12,5 +13,6 @@ const router = express.Router()
 router.post("/", verifyToken, createComplaint)
 router.get("/", verifyToken, getComplaintsByUser)
 router.delete("/", verifyToken, deleteComplaint)
+router.get("/:id", verifyToken, getComplaintsByBusiness)
 
 export default router;
