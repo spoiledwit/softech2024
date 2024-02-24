25ec9780d3e0d39993210c5f05bb68ef82a8e996
Ahmad Ashfaq <spoiledwit@gmail.com>
2024-02-24 20:28:50 +0500

    nasdksadmskldmlsamdlkasmadsad"

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,4 +1,4 @@
-import { login, register, getUser, forgotPassword, toggleWishlistItem } from "../controllers/Auth.js";
+import { login, register, getUser, forgotPassword, appendToWishlist, removeFromWishlist, updateUser } from "../controllers/Auth.js";
 import verifyToken from "../middlewares/verifyToken.js";
 import express from "express";
 
@@ -6,8 +6,10 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/", verifyToken, updateUser)
 router.put("/forgot", forgotPassword);
 router.get("/user", verifyToken, getUser);
-router.post("/wishlist/:itemId", verifyToken, toggleWishlistItem);
+router.post("/wishlist/append/:itemId", verifyToken, appendToWishlist);
+router.post("/wishlist/remove/:itemId", verifyToken, removeFromWishlist);
 
 export default router;
