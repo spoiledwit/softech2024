4d3b20a0232d19b514a2e62c5185fe19c973420e
bilal-kun <khawajabilal902@gmail.com>
2024-02-24 13:44:09 +0500

    added navbar dummy routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,6 +31,10 @@ const App = () => {
         <Route path="item/:id" element={<Item />} />
         <Route path="forgot-password" element={<ForgotPass />} />
         <Route path="panel" element={<BusinessLayout />} />
+        <Route path="historical-sites" element={<Home />} />
+        <Route path="natural-wonders" element={<Home/>} />
+        <Route path="cultural-attractions" element={<Home />} />
+        <Route path="adventure-spots" element={<Home />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Route>
     </Routes>
