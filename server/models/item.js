abaf17049b136d04e722a36c6e3f8b7e65e6d25a
Ahmad Ashfaq <spoiledwit@gmail.com>
2024-02-24 13:19:43 +0500

    Asd

--- server/models/item.js
+++ server/models/item.js
@@ -1,37 +1,51 @@
 import mongoose from "mongoose";
 
-const ItemSchema = new mongoose.Schema({
-  businessId: {
-    type: mongoose.Types.objectId, ref: "Item",
-    required: true
-  },
-  title: { type: String, required: true },
-  category: { type: String, enum: ["historical_sights", "natural_wonders", "cultural_attractions", "adeventure_spots"], required: true },
-  city: { type: String, required: true },
-  images: { type: [String], required: true },
-  videos: { type: [String], required: true },
-  content: [
-    {
-      title: { type: String, required: true },
-      markdown: { type: String, required: true },
+const ItemSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    images: { type: [String], required: true },
+    videos: { type: [String], required: true },
+    businessId: {
+      type: String,
     },
-  ],
-  price: { type: Number, required: true },
-  reviews: [
-    {
-      user_id: { type: String, required: true },
-      review: { type: String, required: true },
-      rating: { type: Number, required: true },
+    content: [
+      {
+        title: { type: String, required: true },
+        markdown: { type: String, required: true },
+      },
+    ],
+    location: {
+      lan: {
+        type: Number,
+        required: true,
+      },
+      lng: {
+        type: Number,
+        required: true,
+      },
+      region: {
+        type: String,
+        required: true,
+      },
     },
-  ],
-  available_dates: {
-    all_available: { type: Boolean, required: true },
-    dates: { type: [Date], required: true },
+    price: { type: Number, required: true },
+    reviews: [
+      {
+        user_id: { type: String, required: true },
+        review: { type: String, required: true },
+        rating: { type: Number, required: true },
+      },
+    ],
+    available_dates: {
+      all_available: { type: Boolean, required: true },
+      dates: { type: [Date], required: true },
+    },
   },
-},
   {
     timestamps: true,
   }
 );
 
-export default mongoose.models.Item || mongoose.model("Item", ItemSchema);
+const Item = mongoose.model("Item", ItemSchema);
+export default Item;
