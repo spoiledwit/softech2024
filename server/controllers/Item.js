5d2988d1dea45e9c21f424c83b534f98cfb2f8a8
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-02-24 19:41:50 +0500

    added item review on backend

--- server/controllers/Item.js
+++ server/controllers/Item.js
@@ -1,4 +1,4 @@
-import Item from "../models/Item.js";
+import Item from "../models/item.js";
 
 export const createItem = async (req, res) => {
   const {
@@ -69,3 +69,23 @@ export const getItemByBusiness = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+export const addReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { review, rating } = req.body;
+    const userId = req.userId;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).send(`No item with id: ${id}`);
+    }
+    const item = Item.findById(id);
+    if (!item) {
+      return res.status(404).send("Item not found");
+    }
+    item.reviews.push({ userId, review, rating });
+    await item.save();
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
